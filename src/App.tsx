bdd0629efb7bb04581bbdb2b27848f3c1b88a9aa
Julie Moura <18290320701@cefet-rj.br>
2026-06-09 01:13:29 -0300

    chore: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AppRoutes from "@/routes/AppRoutes";
+
+function App() {
+  return (
+    <div className="h-screen w-screen overflow-hidden">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
